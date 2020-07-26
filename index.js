7558102e2352f16290341ba569d8877beee0f3d9
danielle revels <ddaniellerrevels@aol.com>
2020-07-26 17:24:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-var greeting = `Hello, everybody!`
-var specialGuest = `Neil deGrasse Tyson` `Hello,` + specialGuest + `!`
+var greeting = `Hello, everybody!`;
+var specialGuest = `Neil deGrasse Tyson`
+var greetSpecialGuest = `` + specialGuest + `!`;
+var topic = `space`;
+var conversation = `${topic}`;
